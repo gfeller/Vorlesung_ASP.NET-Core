f5eec23154284381254e6f40db10047d008446ba
Michael Gfeller <mgfeller@hsr.ch>
2018-03-21 08:39:42 +0100

    files moved

---
+++ Uebung/loesung/Uebung/Models/Bmi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Uebung.Models
+{
+    public class Bmi
+    {
+        [Range(0, 300)]
+        [Display(Name = "Gewicht in kg")]
+        public double Weight { get; set; }
+
+        [Range(30, 250)]
+        [Display(Name = "Höhe in cm")]
+        public double Height { get; set; }
+    }
+}
---
+++ Uebung/loesung/Uebung/Services/BmiService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Uebung.Models;
+
+namespace Uebung.Services
+{
+    public interface IBmiService
+    {
+        double Calculcate(Bmi data);
+    }
+
+    public class BmiService : IBmiService
+    {
+        public double Calculcate(Bmi data)
+        {
+            return Math.Round(data.Weight/Math.Pow((data.Height/100), 2),2);
+        }
+    }
+}
