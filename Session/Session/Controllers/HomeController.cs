7e4af99db7e5ebddf37cda6fcc141b42e5b3b815
Michael Gfeller <mgfeller@hsr.ch>
2018-03-21 07:19:00 +0100

    add session demo

---
+++ Session/Session/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string SesssionKey = "Key";
+        public IActionResult Index()
+        {
+            var value = (HttpContext.Session.GetInt32(SesssionKey) ?? 0) + 1;
+            HttpContext.Session.SetInt32(SesssionKey, value);
+            return Content($"Dein {value}-xter Besuch");
+        }
+    }
+}
---
+++ Session/Session/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Session
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
