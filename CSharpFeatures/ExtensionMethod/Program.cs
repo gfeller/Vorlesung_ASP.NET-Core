3e73b2f8fa82d35bc1da4cf0a2d716809ee9d99f
Michael Gfeller <mgfeller@hsr.ch>
2018-04-26 06:52:57 +0200

    C# Features Update

---
+++ CSharpFeatures/ExtensionMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+
+    public static class MyExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' }).Length;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Extension Methods".WordCount());
+            Console.ReadLine();
+        }
+    }
+}
